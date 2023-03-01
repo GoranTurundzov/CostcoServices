567eec7a367501a8dad6ff346a4c49db0c1ffeed
GoranTurundzov <Goran.Turundzov1@gmail.com>
2023-03-01 10:39:34 +0100

    ...

---
+++ TimesheetEntry/ContosoServicesIntegration/OAuthService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ContosoServicesIntegration
+{
+    public class OAuthService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _clientId;
+        private readonly string _clientSecret;
+        private readonly string _resource;
+        private const string _oauthEndpoint = "https://login.windows.net/be-terna.com/oauth2/token";
+
+        public OAuthService(HttpClient httpClient, string clientId, string clientSecret, string resource)
+        {
+            _httpClient = httpClient;
+            _clientId = clientId;
+            _clientSecret = clientSecret;
+            _resource = resource;
+        }
+
+        public async Task<string> GetAccessTokenAsync()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Post, _oauthEndpoint);
+            var body = $"resource={_resource}&grant_type=client_credentials&client_id={_clientId}&client_secret={_clientSecret}";
+            request.Content = new StringContent(body, Encoding.UTF8, "application/x-www-form-urlencoded");
+
+            var response = await _httpClient.SendAsync(request);
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception($"Failed to get access token: {response.ReasonPhrase}");
+            }
+
+            var content = await response.Content.ReadAsStringAsync();
+            var tokenResponse = JsonSerializer.Deserialize<OAuthTokenResponse>(content);
+            return tokenResponse.AccessToken;
+        }
+    }
+
+    public class OAuthTokenResponse
+    {
+        [JsonPropertyName("access_token")]
+        public string AccessToken { get; set; }
+    }
+}
---
+++ TimesheetEntry/ContosoServicesIntegration/Program.cs
@@ -0,0 +1,81 @@
+﻿using Newtonsoft.Json.Linq;
+using ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Mail;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Threading.Tasks;
+
+namespace ContosoServicesIntegration
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var clientId = "f5104fd2-6af1-4a52-8e18-97ac5102c9fc";
+            var clientSecret = "4Mq8Q~SYLjYYV6P1WulHy.SKtATqULXZWx8MHccT";
+            var resource = "00000015-0000-0000-c000-000000000000";
+            var oauthUrl = "https://login.windows.net/be-terna.com/oauth2/token";
+
+            // Get access token
+            var httpClient = new HttpClient();
+            var oauthService = new OAuthService(httpClient, clientId, clientSecret, resource);
+            var accessToken = await oauthService.GetAccessTokenAsync();
+
+            // Create HTTP client with default headers
+           
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            TSTimesheetSubmissionServiceClient client = new TSTimesheetSubmissionServiceClient();
+
+            HttpRequestMessageProperty requestProperty = new HttpRequestMessageProperty();
+            requestProperty.Headers["Authorization"] = "Bearer " + accessToken;
+
+            OperationContextScope contextScope = new OperationContextScope(client.InnerChannel);
+            OperationContext.Current.OutgoingMessageProperties[HttpRequestMessageProperty.Name] = requestProperty;
+
+
+
+            var arr = new List<TsTimesheetEntry>()
+            {
+
+                   new TsTimesheetEntry() {
+
+                        parmResource =  5637145078,
+                        parmProjectDataAreaId = "USSI",
+                        parmProjId = "00000101",
+                        parmProjActivityNumber = "W00002480",
+                        parmEntryDate = new DateTime(2019,9,4,10,10,12),
+                        parmHrsPerDay = 4
+                    }
+                  
+            }.ToArray();
+
+            
+    
+            
+
+            TSTimesheetEntryList tSTimesheetEntryList = new TSTimesheetEntryList();
+            tSTimesheetEntryList.__k_parmEntryList = arr;
+
+            var callContext = new CallContext()
+            {
+                Company = "Contoso Services International,",
+            };
+
+            var res = await client.createOrUpdateTimesheetLineAsync(new CallContext(), tSTimesheetEntryList);
+
+
+     
+            client.Close();
+
+           
+         
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
